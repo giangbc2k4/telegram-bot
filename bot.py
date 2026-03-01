d4bccaea6e441b65b098e01b8e010a9f4e45574c
JangTran <114843838+giangbc2k4@users.noreply.github.com>
2026-03-01 19:57:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,11 @@ TOKEN = os.getenv("BOT_TOKEN")
 SHEET_CSV = "https://docs.google.com/spreadsheets/d/1GjDK8knbs-JuPNsP125vqEXmoIBb9Pu_4kFAOATCEuA/export?format=csv&gid=0"
 
 
+# ================= GET CHAT ID =================
+async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Chat ID của bạn là: {update.effective_chat.id}")
+
+
 # ================= LOAD DATA =================
 def load_data():
     df = pd.read_csv(SHEET_CSV)
@@ -29,7 +34,6 @@ def load_data():
 
     df["Start"] = pd.to_datetime(df["Start"], dayfirst=True, errors="coerce")
     df["End"] = pd.to_datetime(df["End"], dayfirst=True, errors="coerce")
-
     df["ChatID"] = df["ChatID"].astype(str)
 
     return df
@@ -39,7 +43,6 @@ def load_data():
 async def draw_timeline(update: Update, context: ContextTypes.DEFAULT_TYPE, target_date):
 
     chat_id = str(update.effective_chat.id)
-
     df = load_data()
 
     df = df[df["ChatID"] == chat_id]
@@ -94,6 +97,7 @@ async def timeline_yesterday(update: Update, context: ContextTypes.DEFAULT_TYPE)
 # ================= MAIN =================
 app = ApplicationBuilder().token(TOKEN).build()
 
+app.add_handler(CommandHandler("getid", get_id))
 app.add_handler(CommandHandler("timeline_today", timeline_today))
 app.add_handler(CommandHandler("timeline_yesterday", timeline_yesterday))
 
